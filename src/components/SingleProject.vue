c795f44eff4196955aa6e8d2ecd57ff48a650084
CKMolinga <ck.molinga@gmail.com>
2022-04-08 01:50:17 +0200

    initial commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="project">
+      <div class="actions">
+          <h3 @click="toggleDetails"> {{ project.title }}</h3>
+          <div class="icons">
+              <span class="material-icons">edit</span>
+              <span class="material-icons">delete</span>
+              <span class="material-icons">done</span>
+          </div>
+      </div>
+        <div class="description">
+                <p v-if="showDetails">{{ project.description }}</p>
+        </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showDetails: false,
+        }
+    },
+props: ['project'],
+methods: {
+    // toggle paragraph visibility
+    toggleDetails() {
+        this.showDetails = !this.showDetails;
+    }  
+},
+}
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  max-width: 600px;
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  border-left: 4px solid #e90074;
+}
+h3 {
+  font-size: 1.5em;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.material-icons {
+  font-size: 1.5em;
+  cursor: pointer;
+  margin-left: 10px;
+  color: #bbb;
+}
+
+.material-icons:hover {
+  color: #e90074;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"></SingleProject>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from '../components/SingleProject.vue'
+
+export default {
+  name: 'Home',
+  components: {SingleProject},
+  data () {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(response => response.json())
+      .then(data => {
+        this.projects = data;
+      })
+      .catch(error => console.error(error))
+  }
+}
+</script>
